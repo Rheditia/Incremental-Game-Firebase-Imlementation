476350c675194c391dc76f34128ff394fa81847a
Rheditia <rheditiaf2406@gmail.com>
2021-12-19 10:57:26 +0700

    Finished step 1

--- Incremental Game Firebase/Assets/Scripts/ResourceController.cs
+++ Incremental Game Firebase/Assets/Scripts/ResourceController.cs
@@ -11,7 +11,29 @@ public class ResourceController : MonoBehaviour
 
     private ResourceConfig _config;
 
-    private int _level = 1;
+    private int _index;
+    private int _level
+    {
+        set
+        {
+            // Menyimpan value yang di set ke _level pada Progress Data
+            UserDataManager.Progress.ResourcesLevels[_index] = value;
+            UserDataManager.Save();
+        }
+
+        get
+        {
+            // Mengecek apakah index sudah terdapat pada Progress Data
+            if (!UserDataManager.HasResources(_index))
+            {
+                // Jika tidak maka tampilkan level 1
+                return 1;
+            }
+
+            // Jika iya maka tampilkan berdasarkan Progress Data
+            return UserDataManager.Progress.ResourcesLevels[_index];
+        }
+    }
 
     public bool IsUnlocked { get; private set; }
 
@@ -30,16 +52,17 @@ public class ResourceController : MonoBehaviour
         });
     }
 
-    public void SetConfig (ResourceConfig config)
+    public void SetConfig (int index, ResourceConfig config)
     {
+        _index = index;
         _config = config;
 
         // ToString("0") berfungsi untuk membuang angka di belakang koma
-        ResourceDescription.text = $"{ _config.Name } Lv. { _level }\n+{ GetOutput ().ToString ("0") }";
+        ResourceDescription.text = $"{ _config.Name } Lv. { _level }\n+{ GetOutput().ToString("0") }";
         ResourceUnlockCost.text = $"Unlock Cost\n{ _config.UnlockCost }";
-        ResourceUpgradeCost.text = $"Upgrade Cost\n{ GetUpgradeCost () }";
+        ResourceUpgradeCost.text = $"Upgrade Cost\n{ GetUpgradeCost() }";
 
-        SetUnlocked (_config.UnlockCost == 0);
+        SetUnlocked(_config.UnlockCost == 0 || UserDataManager.HasResources(_index));
     }
 
     public double GetOutput ()
@@ -59,37 +82,47 @@ public class ResourceController : MonoBehaviour
 
     public void UpgradeLevel ()
     {
-        double upgradeCost = GetUpgradeCost ();
-        if (GameManager.Instance.TotalGold < upgradeCost)
+        double upgradeCost = GetUpgradeCost();
+        if (UserDataManager.Progress.Gold < upgradeCost)
         {
             return;
         }
 
-        GameManager.Instance.AddGold (-upgradeCost);
+        GameManager.Instance.AddGold(-upgradeCost);
         _level++;
 
-        ResourceUpgradeCost.text = $"Upgrade Cost\n{ GetUpgradeCost () }";
-        ResourceDescription.text = $"{ _config.Name } Lv. { _level }\n+{ GetOutput ().ToString ("0") }";
+        ResourceUpgradeCost.text = $"Upgrade Cost\n{ GetUpgradeCost() }";
+        ResourceDescription.text = $"{ _config.Name } Lv. { _level }\n+{ GetOutput().ToString("0") }";
     }
 
     public void UnlockResource ()
     {
-        double unlockCost = GetUnlockCost ();
-        if (GameManager.Instance.TotalGold < unlockCost)
+        double unlockCost = GetUnlockCost();
+        if (UserDataManager.Progress.Gold < unlockCost)
         {
             return;
         }
 
-        SetUnlocked (true);
-        GameManager.Instance.ShowNextResource ();
-        AchievementController.Instance.UnlockAchievement (AchievementType.UnlockResource, _config.Name);
+        SetUnlocked(true);
+        GameManager.Instance.ShowNextResource();
+        AchievementController.Instance.UnlockAchievement(AchievementType.UnlockResource, _config.Name);
     }
 
     public void SetUnlocked (bool unlocked)
     {
         IsUnlocked = unlocked;
+        if (unlocked)
+        {
+            // Jika resources baru di unlock dan belum ada di Progress Data, maka tambahkan data baru
+            if (!UserDataManager.HasResources(_index))
+            {
+                UserDataManager.Progress.ResourcesLevels.Add(_level);
+                UserDataManager.Save();
+            }
+        }
+
         ResourceImage.color = IsUnlocked ? Color.white : Color.grey;
-        ResourceUnlockCost.gameObject.SetActive (!unlocked);
-        ResourceUpgradeCost.gameObject.SetActive (unlocked);
+        ResourceUnlockCost.gameObject.SetActive(!unlocked);
+        ResourceUpgradeCost.gameObject.SetActive(unlocked);
     }
 }
